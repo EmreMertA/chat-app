d551a70ea5ea0d7136c480423b471e5e1977f6c1
EmreMertA <emremakdag55@gmail.com>
2022-05-26 19:30:52 +0300

    firebase update

--- src/components/ChatPanel/Message.jsx
+++ src/components/ChatPanel/Message.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import moment from "moment";
-import styles from "./messages.module.css";
-import { Comment, Image } from "semantic-ui-react";
+import React from 'react';
+import moment from 'moment';
+import styles from './messages.module.css';
+import { Comment, Image } from 'semantic-ui-react';
 
 const Message = ({ message }) => {
   const timeFromNow = (timestamp) => moment(timestamp).fromNow();
 
-  const isMedia = (message) => message.hasOwnProperty("image");
+  const isMedia = (message) => message.hasOwnProperty('image');
 
   return (
     <Comment>
       <Comment.Avatar src={message.user.avatar} />
       <Comment.Content>
-        <Comment.Author as="a">{message.user.name}</Comment.Author>
+        <Comment.Author as='a'>{message.user.name}</Comment.Author>
         <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
         {isMedia(message) ? (
-          <a href={message.image} target="_blank" className={styles.image}>
-            <Image src={message.image} className={styles.image} />
+          <a href={message.image} target='_blank' className={styles.image}>
+            <Image src={message.image} style={{ width: '250px' }} />
           </a>
         ) : (
           <Comment.Text>{message.content}</Comment.Text>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Adl_Z4hbi6RZlqeBhohn45FyAyQ0D4k",
-  authDomain: "chat-app-7748f.firebaseapp.com",
-  projectId: "chat-app-7748f",
-  storageBucket: "chat-app-7748f.appspot.com",
-  messagingSenderId: "424549383539",
-  appId: "1:424549383539:web:b9678fc92cceda77e99906",
-  measurementId: "G-VL11KYBY7Q",
+  apiKey: "AIzaSyAeWXPtK64tIuwe4eaLVLTtBM6diUSbs54",
+  authDomain: "chat-app-df5e2.firebaseapp.com",
+  databaseURL: "https://chat-app-df5e2-default-rtdb.firebaseio.com",
+  projectId: "chat-app-df5e2",
+  storageBucket: "chat-app-df5e2.appspot.com",
+  messagingSenderId: "96852704576",
+  appId: "1:96852704576:web:f9a72d54866d39f9dccea5",
+  measurementId: "G-9CR769CQ6Q"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
